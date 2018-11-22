c3c2b629265e5ad15afe5149db07e2967b30ffee
coder <nompetsheniayabonga@gmail.com>
2018-11-22 17:32:25 +0200

    vue js front end

---
+++ src/components/blog.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="blog">
+		<h1>{{title}}</h1>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-header">
+						<h3>LenderName</h3>
+					</div>
+					<div class="card-body">
+						
+					<p>						describe your Application
+</p>
+
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-header">
+						<h3>Lender Name</h3>
+					</div>
+					<div class="card-body">
+						
+<p>						describe your Application
+</p>
+
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-header">
+						<h3>Lender Name</h3>
+					</div>
+					<div class="card-body">
+						
+						<p>describe your Application</p>
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+  name: "TopRated Projects",
+  data() {
+    return {
+      title: "Top Lenders"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <h1>{{title}}</h1>
+          <h2>Why Us ?</h2>
+          {{why}}
+<div class="row">
+  
+</div>
+    <div class="row">
+
+      <div class="col-md-4 col-lg4" v-for="(data,index) in products" :key="index"> Front-End
+        <img :src="data.image" class="img-fluid">
+         <h3 @click="goTodetail(data.productId)" >{{data.productTitle}}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      title: 'Welcome To Dev Lender ',
+      why: 'because blah blah blah',
+      products:[
+      {
+        productTitle:"for more details click here",
+        image       : require('../assets/images/product.jpg'),
+        productId:1
+      },
+      
+      ]
+    }
+  },
+  methods:{
+  goTodetail(prodId) {
+    let proId=prodId
+    this.$router.push({name:'details',params:{Pid:proId}})
+  }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.row img{
+  max-height: 15em;
+  width: 100%;
+
+}
+.row h3{
+  cursor:pointer;
+}
+</style>
